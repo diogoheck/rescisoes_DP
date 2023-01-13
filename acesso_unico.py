11391b553bcb45602b8a8e9f5df36f144a6f1261
diogoheck <diogo.heck.dev@gmail.com>
2023-01-13 09:02:23 -0300

    esboco_projeto

---
+++ acesso_unico.py
@@ -0,0 +1,34 @@
+# relatorios unico
+
+# 1
+# calculos (alt + c)
+# folha normal individual (ctrl + N)
+# selecionar empresa (2031)
+# selecionar colaborador (ANELISE - 3124)
+# colocar competencia
+# enter
+# imprimir
+# portaria MTE
+# visualizar
+# salvar (icone excel)
+# adobe
+# salvar U 
+# renomear "TRCT" e transferir para pasta O:\Clientes\Kuchak\Dpto Pessoal\2023\2031 MATRIZ\012023\RESCISÕES\RES ANELISE TOEBE
+
+# 2 (igual ao 1)
+# mudar o modelo do relatório
+# verbas por classificacao com valores para GRRF
+# visualizar
+# salvar (icone excel)
+# adobe
+# salvar U
+# renomear "ANALITICO" e transferir para pasta O:\Clientes\Kuchak\Dpto Pessoal\2023\2031 MATRIZ\012023\RESCISÕES\RES ANELISE TOEBE
+
+# 3
+# relatórios
+# periódicos
+# ficha registro auxiliar
+# selecionar colaborador
+# salvar "FICHA AUXILIAR"
+
+# JUNTAR TODOS PDFS (VALIDAR CNPJ ANTES) -> O:\Clientes\Kuchak\Dpto Pessoal\2023\2031 MATRIZ\012023\RESCISÕES\RES ANELISE TOEBE
---
+++ conectividade_social.py
@@ -0,0 +1,19 @@
+# entrar no conectividade.caixa.gov.br
+# buscar certificado luciano
+# selecione empregador
+# selecione certificado luciano
+# acessar empresa outorgante
+# CNPJ da empresa ortorgante
+# continuar
+# selecione 
+# solicitar extrato do trabalhador
+# rs - rio grande do sul
+# colocar o pis
+# continuar
+# salvar extrato na pasta "EXTRATO.pdf"
+# voltar
+# selecione comunicação de dispensa
+# colocar o pis
+# data do desligamento
+# modalidade da rescisão
+# salvar documento na pasta "CHAVE DE MOVIMENTAÇAO"
